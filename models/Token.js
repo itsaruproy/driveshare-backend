fa40db4dcf004b23fb2b34915c41a8d8d02d54e4
roy <89403308+itsaruproy@users.noreply.github.com>
2023-01-14 17:52:11 +0530

    feat: deleteing token after 6 days

--- models/Token.js
+++ models/Token.js
@@ -1,28 +1,50 @@
-const tokensCollection = require("../db").db().collection("tokens")
+const tokensCollection = require('../db').db().collection('tokens')
 
 class Token {
     constructor(gid, refreshToken) {
         this.gid = gid
         this.refreshToken = refreshToken
+        this.tokenDate = new Date()
     }
 
     save = () => {
         return new Promise(async (resolve, reject) => {
             try {
-                await tokensCollection.insertOne({gid: this.gid, refreshToken: this.refreshToken})
+                await tokensCollection.insertOne({
+                    gid: this.gid,
+                    refreshToken: this.refreshToken,
+                    tokenDate: this.tokenDate,
+                })
                 resolve()
-            } catch(err) {
+            } catch (err) {
                 reject(err)
             }
         })
     }
 
-    static findTokenByUserID = (userID) => {
+    static findTokenByUserID = userID => {
         return new Promise(async (resolve, reject) => {
             try {
-                const tokenDetails = await tokensCollection.findOne({gid: userID})
+                const tokenDetails = await tokensCollection.findOne({
+                    gid: userID,
+                })
                 resolve(tokenDetails)
-            } catch(err) {
+            } catch (err) {
+                reject(err)
+            }
+        })
+    }
+    static deleteTokenByUserID = userID => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const deletedToken = await tokensCollection.deleteOne({
+                    gid: userID,
+                })
+                if (deletedToken.deletedCount === 0) {
+                    throw new Error(`No token found for user ID: ${userID}`)
+                }
+                resolve(deletedToken)
+            } catch (err) {
                 reject(err)
             }
         })
